2e33fe9ab6bee5c223ca4bedca4866055cc2b5ca
tuturlavoiture29 <charvetarthur42@gmail.com>
2024-12-18 22:57:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function checkPassword() {
   const password = document.getElementById("password").value;
   const errorMessage = document.getElementById("error-message");
 
-  if (password === "notreSecret") {
-    window.location.href = "invitation.html"; // Redirige vers la page d'invitation
+  if (password === "caca") {
+    window.location.href = "page1.html"; // Redirige vers la page d'invitation
   } else {
     errorMessage.textContent = "Mot de passe incorrect !";
   }
